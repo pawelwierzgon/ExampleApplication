264a3188b6c96723e42b7d7f7ae3506ca0f6b96c
Paweł Wierzgoń <pawel.wierzgon@gmail.com>
2024-04-07 02:36:10 +0100

    Fix vulnerabilities showed by owasp zap

--- routes/math.js
+++ routes/math.js
@@ -1,61 +1,133 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var utils = require('../modules/utils')
+var utils = require("../modules/utils");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.set(
+    "Content-Security-Policy",
+    "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'"
+  );
   res.redirect("/math/add");
-
 });
-
 
 /* GET home page. */
-router.get('/add', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
-      result = utils.add(parseFloat(num1), parseFloat(num2))
+router.get("/add", function (req, res, next) {
+  var result = 0;
+  var { num1, num2 } = req.query;
+  if (num1 && num2) {
+    result = utils.add(parseFloat(num1), parseFloat(num2));
   }
-  res.render('compute', { title: 'add' ,result:result, num1, num2, symbol:"+" });
-
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.set(
+    "Content-Security-Policy",
+    "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'"
+  );
+  res.render("compute", {
+    title: "add",
+    result: result,
+    num1,
+    num2,
+    symbol: "+",
+  });
 });
 
-router.get('/divide', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
+router.get("/divide", function (req, res, next) {
+  var result = 0;
+  var { num1, num2 } = req.query;
+  if (num1 && num2) {
     try {
-      result = utils.divide(parseFloat(num1), parseFloat(num2))
+      result = utils.subtract(parseFloat(num1), parseFloat(num2));
     } catch (e) {
-      result = "error"
+      result = "error";
     }
-      
   }
-  res.render('compute', { title: 'divide' ,result:result, num1, num2, symbol:"/" });
-
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.set(
+    "Content-Security-Policy",
+    "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'"
+  );
+  res.render("compute", {
+    title: "divide",
+    result: result,
+    num1,
+    num2,
+    symbol: "/",
+  });
 });
 
-router.get('/multiply', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  if(num1 !=='' && num2 !=='') {
-      result = utils.multiply(parseFloat(num1), parseFloat(num2))
+router.get("/multiply", function (req, res, next) {
+  var result = 0;
+  var { num1, num2 } = req.query;
+  if (num1 && num2) {
+    result = utils.multiply(parseFloat(num1), parseFloat(num2));
   }
-  res.render('compute', { title: 'multiply' ,result:result, num1, num2, symbol:"*" });
-
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.set(
+    "Content-Security-Policy",
+    "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'"
+  );
+  res.render("compute", {
+    title: "multiply",
+    result: result,
+    num1,
+    num2,
+    symbol: "*",
+  });
 });
 
-
-router.get('/subtract', function(req, res, next) {
-  var result = 0
-  var {num1, num2} = req.query
-  console.log(req.query)
-  if(num1 !=='' && num2 !=='') {
-      result = utils.subtract(parseFloat(num1), parseFloat(num2))
+router.get("/subtract", function (req, res, next) {
+  var result = 0;
+  var { num1, num2 } = req.query;
+  if (num1 && num2) {
+    result = utils.divide(parseFloat(num1), parseFloat(num2));
   }
-  res.render('compute', { title: 'subtract' ,result:result, num1, num2, symbol:"-" });
-
+  console.log(result);
+  console.log(num1);
+  console.log(num2);
+  res.set("X-Frame-Options", "DENY");
+  res.set("X-Content-Type-Options", "nosniff");
+  res.set(
+    "Strict-Transport-Security",
+    "max-age=31536000; includeSubDomains; preload"
+  );
+  res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+  res.set(
+    "Content-Security-Policy",
+    "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'"
+  );
+  res.render("compute", {
+    title: "subtract",
+    result: result,
+    num1,
+    num2,
+    symbol: "-",
+  });
 });
-
 
 module.exports = router;
